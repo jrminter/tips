5ee0874b5d1c48e5ce5442af5e9ca15d04449bdb
John Minter <jrminter@rochester.rr.com>
2013-12-17 20:55:51 -0500

    updated build script

--- scripts/knit-them-all.R
+++ scripts/knit-them-all.R
@@ -4,28 +4,35 @@ rm(list=ls())
 require(knitr)
 
 strGitHome <- Sys.getenv("GIT_HOME")
+if(strGitHome=="") strGitHome="~/git"
 strRel     <- '/tips/scripts'
 
-names <- c("automater.Rmd", "C++11-Tips.Rmd", "color.Rmd", "DiffractionLimit.Rmd",
+names <- c("automater.Rmd", "C++11-Tips.Rmd",
+           "color.Rmd", "DiffractionLimit.Rmd",
            "EDAX.Rmd", "git-tips.Rmd","gnuplot.Rmd",
            "ImageJ.Rmd", "Legacy.Rmd", "mac.Rmd", 
            "micro.Rmd", "mysql.Rmd", "plagiarism.Rmd",
            "python.Rmd","R-tips.Rmd", 
-           "R-Anova.Rmd", "R-bar-plots","R-foreach.Rmd", "R-GeoSpatial.Rmd",
+           "R-Anova.Rmd", "R-bar-plots.Rmd",
+           "R-foreach.Rmd", "R-GeoSpatial.Rmd",
            "Salabim-graphics.Rmd", "shell-tips.Rmd",
            "skimage.Rmd","Slidify.Rmd",
            "Sweave.Rmd", "tex-tips.Rmd",
            "win.Rmd","README.Rmd")
-rPath <- c("automater", "C++11", "../color", "../DiffractionLimit", 
+rPath <- c("../automater", "../C++11",
+           "../color", "../DiffractionLimit", 
            "../EDAX", "../git", "../gnuplot", "../ImageJ",
            "../legacy", "../mac","../micro", "../mysql",
            "../plagiarism", "../python", "../R",
-           "../R-Anova", "../R-bar-plots" ,"../R-foreach", "../R-GeoSpatial",
-           "../Salabim", "../skimage", "../Slidify",
-           "../shell", "../Sweave",
+           "../R-Anova", "../R-bar-plots" ,
+           "../R-foreach", "../R-GeoSpatial",
+           "../Salabim", "../shell", 
+           "../skimage", "../Slidify",
+           "../Sweave",
            "../tex", "../win", "../")
 
 knitIt <- function(wrkPath, scrPath, theName){
+  print(wrkPath)
   setwd(wrkPath)
   knit2html(theName)
   theMD <- paste0(strsplit(theName, ".Rmd")[[1]], ".md")
@@ -34,6 +41,7 @@ knitIt <- function(wrkPath, scrPath, theName){
 }
 
 strScripts <- paste0(strGitHome, strRel)
+print(strScripts)
 
 
 
