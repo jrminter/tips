af07c89a9f7746817c856314f1420ab2e54dd338
John Minter <john.r.minter@kodak.com>
2014-11-18 08:50:57 -0500

    added a typical pull all script to scripts

---
+++ scripts/pull-all-git.sh
@@ -0,0 +1,161 @@
+#! /bin/bash
+# Pull all git repos
+# J. R. Minter
+# version of 2014-11-18
+
+
+echo CMCI
+
+cd /C/Users/l837410/Documents/git/cmci/directionalityPractical
+echo directionalityPractical
+git pull
+echo   
+
+cd /C/Users/l837410/Documents/git/cmci/HTManalysisCourse
+echo HTManalysisCourse
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/cmci/ij_textbook1
+echo ij_textbook1
+git pull
+echo 
+
+cd /C/Users/l837410/Documents/git/cmci/ij_textbook2
+echo ij_textbook2
+git pull
+echo 
+
+cd /C/Users/l837410/Documents/git/cmci/ijmacros
+echo ijmacros
+git pull
+echo 
+
+cd /C/Users/l837410/Documents/git/cmci/Scripts
+echo Scripts
+git pull
+echo 
+
+cd /C/Users/l837410/Documents/git/cmci/TrackingLecture
+echo TrackingLecture
+git pull
+echo   
+
+echo Main
+
+echo  
+
+cd /C/Users/l837410/Documents/git/courses
+echo courses
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/cython
+echo cython
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/data_and_truth
+echo data_and_truth
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/edp
+echo edp
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/hyperspy
+echo hyperspy
+git fetch origin
+git fetch pburdet
+# git branch -r
+git pull
+echo  
+
+
+cd /C/Users/l837410/Documents/git/hyperspy-demos
+echo hyperspy-demos
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/mahotas
+echo mahotas
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/OSImageAnalysis
+echo OSImageAnalysis
+git pull
+echo  
+
+echo  
+cd /C/Users/l837410/Documents/git/python-bootcamp
+echo python-bootcamp
+git pull
+echo  
+
+echo  
+cd /C/Users/l837410/Documents/git/python-seminar
+echo python-seminar
+git pull
+echo 
+
+cd /C/Users/l837410/Documents/git/rpackages
+echo rpackages
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/rWipeTest
+echo rWipeTest
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/rWrapStrataGem
+echo rWrapStrataGem
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/rXEDS
+echo rXEDS
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/scientific-python-lectures
+echo scientific-python-lectures
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/scikit-image
+echo scikit-image
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/scikit-learn
+echo scikit-learn
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/seaborn
+echo seaborn
+git pull
+echo
+
+cd /C/Users/l837410/Documents/git/skimage-tutorials
+echo skimage-tutorials
+git pull
+echo
+
+cd /C/Users/l837410/Documents/git/tips
+echo tips
+git pull
+echo  
+
+cd /C/Users/l837410/Documents/git/
+ls
+
+echo  
+
+read -p "Press [Enter] key to finish..."
+
+
