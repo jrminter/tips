cae377205db9c842237f93b6fd948f77afc93448
John Minter <jrminter@gmail.com>
2018-06-13 21:40:59 -0400

    updated date on _install_pkgs

--- scripts/__install-pkgs.R
+++ scripts/__install-pkgs.R
@@ -7,7 +7,8 @@
 # 2017-11-01 fixed JAVA_HOME issue for R-3.4.2
 # 2017-11-28 added magick from github
 #            https://datascienceplus.com/image-processing-and-manipulation-with-magick-in-r/
-#
+# 2018-06-13 Added rafalib and UsingR to buld matrix algebra
+
 
 if( Sys.info()['sysname'] == "Windows"){
 	
