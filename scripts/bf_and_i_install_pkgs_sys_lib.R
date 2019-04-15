706980ddfb284828f34d144f283ca2eb8e0d312a
jrminter <jrminter@gmail.com>
2019-04-15 18:00:10 -0400

    version to the system library

---
+++ scripts/bf_and_i_install_pkgs_sys_lib.R
@@ -0,0 +1,132 @@
+# Start with the tidyverse
+install.packages('devtools', '/usr/lib/R/library')
+install.packages('tidyverse', '/usr/lib/R/library')
+install.packages('svglite', '/usr/lib/R/library')
+install.packages('ggvis', '/usr/lib/R/library')
+install.packages('ggmap', '/usr/lib/R/library')
+install.packages('ggthemes', '/usr/lib/R/library')
+install.packages('Quandl', '/usr/lib/R/library')
+install.packages('extrafont', '/usr/lib/R/library')
+install.packages('Rd2roxygen', '/usr/lib/R/library')
+install.packages('tidyquant', '/usr/lib/R/library')
+install.packages('here', '/usr/lib/R/library') # by Kirill Muller recommended by HW and JB!
+install.packages('fs', '/usr/lib/R/library')
+install.packages('reprex', '/usr/lib/R/library')
+install.packages('UsingR', '/usr/lib/R/library') # data sets including galton
+
+install.packages('ciTools', '/usr/lib/R/library')
+install.packages('reticulate', '/usr/lib/R/library') # R-python interface
+
+# Sweave and pandoc tools
+# install.packages('cacheSweave', '/usr/lib/R/library') # not avail 3.2
+install.packages('knitcitations', '/usr/lib/R/library')
+install.packages('pander', '/usr/lib/R/library')
+install.packages('rticles', '/usr/lib/R/library')
+install.packages('rdformats', '/usr/lib/R/library')
+
+install.packages('binb', '/usr/lib/R/library')
+
+# matrix algebra
+# install.packages('rafalib', '/usr/lib/R/library')
+install.packages('UsingR', '/usr/lib/R/library') # data including basic Galton
+install.packages('HistData', '/usr/lib/R/library') # Has Galton Families
+
+# better number formatting
+install.packages('DescTools', '/usr/lib/R/library')
+
+install.packages('rtiff', '/usr/lib/R/library')
+install.packages('raster', '/usr/lib/R/library')
+install.packages('imager', '/usr/lib/R/library')
+
+# we want revealjs and ReporteRs for presentations
+install.packages('revealjs', '/usr/lib/R/library')
+# install.packages('ReporteRs', '/usr/lib/R/library')
+
+install.packages('RSQLite', '/usr/lib/R/library')
+# install.packages('RMySQL', '/usr/lib/R/library')
+install.packages('pryr', '/usr/lib/R/library')
+install.packages('nycflights13', '/usr/lib/R/library')
+install.packages('RODBC', '/usr/lib/R/library')
+install.packages('Unicode', '/usr/lib/R/library')
+install.packages('TeachingDemos', '/usr/lib/R/library')
+install.packages('plotGoogleMaps', '/usr/lib/R/library')
+install.packages('tikzDevice', '/usr/lib/R/library') # now back on CRAN...
+install.packages('mixtools', '/usr/lib/R/library')
+
+# metrology packages
+install.packages('metRology', '/usr/lib/R/library')
+install.packages('propagate', '/usr/lib/R/library')
+
+# for microR
+install.packages('kernlab', '/usr/lib/R/library')
+install.packages('FactoMineR', '/usr/lib/R/library')
+install.packages('robCompositions', '/usr/lib/R/library')
+install.packages('Cairo', '/usr/lib/R/library')
+install.packages('beanplot', '/usr/lib/R/library')
+install.packages('spatstat', '/usr/lib/R/library')
+install.packages('numbers', '/usr/lib/R/library')
+install.packages('gmp', '/usr/lib/R/library')
+# install.packages('doMC', '/usr/lib/R/library') # not avail 3.3.2
+install.packages('rbenchmark', '/usr/lib/R/library')
+install.packages('doSNOW', '/usr/lib/R/library')
+
+# install.packages('RGtk2', '/usr/lib/R/library')
+# see BFI for mac
+
+install.packages('PerformanceAnalytics', '/usr/lib/R/library')
+
+install.packages('fields', '/usr/lib/R/library')
+# install.packages('h5r', '/usr/lib/R/library') # not avail 3.3.2
+
+install.packages('SuppDists', '/usr/lib/R/library')
+
+install.packages('animation', '/usr/lib/R/library')
+install.packages('highlight', '/usr/lib/R/library')
+install.packages('qualityTools', '/usr/lib/R/library')
+install.packages('rcdk', '/usr/lib/R/library')
+install.packages('rpubchem', '/usr/lib/R/library')
+
+install.packages('rattle', '/usr/lib/R/library')
+install.packages('chemCal', '/usr/lib/R/library')
+install.packages('DAAG', '/usr/lib/R/library')
+install.packages('fftw', '/usr/lib/R/library')
+install.packages('rafalib', '/usr/lib/R/library')
+install.packages('UsingR', '/usr/lib/R/library')
+
+install.packages('fitdistrplus', '/usr/lib/R/library')
+install.packages('mixdist', '/usr/lib/R/library')
+install.packages('histogram', '/usr/lib/R/library')
+install.packages('HSAUR2', '/usr/lib/R/library')
+install.packages('moments', '/usr/lib/R/library')
+install.packages('spc', '/usr/lib/R/library')
+install.packages('sp', '/usr/lib/R/library')
+install.packages('xlsx', '/usr/lib/R/library')
+install.packages('readxl', '/usr/lib/R/library')
+install.packages('DMwR', '/usr/lib/R/library')
+
+install.packages('CORElearn', '/usr/lib/R/library')
+install.packages('corpcor', '/usr/lib/R/library')
+install.packages('ggm', '/usr/lib/R/library')
+install.packages('googleVis', '/usr/lib/R/library')
+install.packages('lasso2', '/usr/lib/R/library')
+install.packages('RXKCD', '/usr/lib/R/library')
+install.packages('ChemometricsWithR', '/usr/lib/R/library')
+# install.packages('ChemometricsWithRData', '/usr/lib/R/library')
+install.packages('chemometrics', '/usr/lib/R/library')
+install.packages('lspls', '/usr/lib/R/library')
+install.packages('FITSio', '/usr/lib/R/library')
+install.packages('LearnBayes', '/usr/lib/R/library')
+install.packages('R2HTML', '/usr/lib/R/library')
+# install.packages('SweaveListingUtils', '/usr/lib/R/library')
+install.packages('mosaic', '/usr/lib/R/library')
+install.packages('latex2exp', '/usr/lib/R/library')
+install.packages('highcharter', '/usr/lib/R/library')
+install.packages('WDI', '/usr/lib/R/library')
+install.packages('tigris', '/usr/lib/R/library')
+install.packages('xaringan', '/usr/lib/R/library')
+install.packages('FinCal', '/usr/lib/R/library')
+install.packages('hrbrthemes', '/usr/lib/R/library')
+install.packages('cowsay', '/usr/lib/R/library')
+# needed for vignettes for ggstatsplot
+install.packages('ordinal', '/usr/lib/R/library')
+install.packages('robust', '/usr/lib/R/library')
