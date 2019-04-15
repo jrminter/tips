13f13d9e585d46e942cc28ac72941054f9c2536d
jrminter <jrminter@DebianSid.localdomain>
2019-04-15 17:38:56 -0400

    a bare script to install pkgs. Try as root for them to end up in system directory

---
+++ scripts/bf_and_i_install_pkgs.R
@@ -0,0 +1,132 @@
+# Start with the tidyverse
+install.packages('devtools')
+install.packages('tidyverse')
+install.packages('svglite')
+install.packages('ggvis')
+install.packages('ggmap')
+install.packages('ggthemes')
+install.packages('Quandl')
+install.packages('extrafont')
+install.packages('Rd2roxygen')
+install.packages('tidyquant')
+install.packages('here') # by Kirill Muller recommended by HW and JB!
+install.packages('fs')
+install.packages('reprex')
+install.packages('UsingR') # data sets including galton
+
+install.packages('ciTools')
+install.packages('reticulate') # R-python interface
+
+# Sweave and pandoc tools
+# install.packages('cacheSweave') # not avail 3.2
+install.packages('knitcitations')
+install.packages('pander')
+install.packages('rticles')
+install.packages('rdformats')
+
+install.packages('binb')
+
+# matrix algebra
+# install.packages('rafalib')
+install.packages('UsingR') # data including basic Galton
+install.packages('HistData') # Has Galton Families
+
+# better number formatting
+install.packages('DescTools')
+
+install.packages('rtiff')
+install.packages('raster')
+install.packages('imager')
+
+# we want revealjs and ReporteRs for presentations
+install.packages('revealjs')
+# install.packages('ReporteRs')
+
+install.packages('RSQLite')
+# install.packages('RMySQL')
+install.packages('pryr')
+install.packages('nycflights13')
+install.packages('RODBC')
+install.packages('Unicode')
+install.packages('TeachingDemos')
+install.packages('plotGoogleMaps')
+install.packages('tikzDevice') # now back on CRAN...
+install.packages('mixtools')
+
+# metrology packages
+install.packages('metRology')
+install.packages('propagate')
+
+# for microR
+install.packages('kernlab')
+install.packages('FactoMineR')
+install.packages('robCompositions')
+install.packages('Cairo')
+install.packages('beanplot')
+install.packages('spatstat')
+install.packages('numbers')
+install.packages('gmp')
+# install.packages('doMC') # not avail 3.3.2
+install.packages('rbenchmark')
+install.packages('doSNOW')
+
+# install.packages('RGtk2')
+# see BFI for mac
+
+install.packages('PerformanceAnalytics')
+
+install.packages('fields')
+# install.packages('h5r') # not avail 3.3.2
+
+install.packages('SuppDists')
+
+install.packages('animation')
+install.packages('highlight')
+install.packages('qualityTools')
+install.packages('rcdk')
+install.packages('rpubchem')
+
+install.packages('rattle')
+install.packages('chemCal')
+install.packages('DAAG')
+install.packages('fftw')
+install.packages('rafalib')
+install.packages('UsingR')
+
+install.packages('fitdistrplus')
+install.packages('mixdist')
+install.packages('histogram')
+install.packages('HSAUR2')
+install.packages('moments')
+install.packages('spc')
+install.packages('sp')
+install.packages('xlsx')
+install.packages('readxl')
+install.packages('DMwR')
+
+install.packages('CORElearn')
+install.packages('corpcor')
+install.packages('ggm')
+install.packages('googleVis')
+install.packages('lasso2')
+install.packages('RXKCD')
+install.packages('ChemometricsWithR')
+# install.packages('ChemometricsWithRData')
+install.packages('chemometrics')
+install.packages('lspls')
+install.packages('FITSio')
+install.packages('LearnBayes')
+install.packages('R2HTML')
+# install.packages('SweaveListingUtils')
+install.packages('mosaic')
+install.packages('latex2exp')
+install.packages('highcharter')
+install.packages('WDI')
+install.packages('tigris')
+install.packages('xaringan')
+install.packages('FinCal')
+install.packages('hrbrthemes')
+install.packages('cowsay')
+# needed for vignettes for ggstatsplot
+install.packages('ordinal')
+install.packages('robust')
