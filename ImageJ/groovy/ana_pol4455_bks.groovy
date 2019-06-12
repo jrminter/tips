88dfeb94794e1c95ed6029a52f4b0a47bcea32c1
John Minter <jrminter@gmail.com>
2019-06-11 22:47:25 -0400

    a better pol4455 grovy analysis

--- ImageJ/groovy/ana_pol4455_bks.groovy
+++ ImageJ/groovy/ana_pol4455_bks.groovy
@@ -2,6 +2,10 @@
 @String(label="Image Name", style="") img_nam
 @String(label="Image Extension", style="") img_tif
 @Integer(label="Gaussian Blurr size") gb_size
+@String(label="min area nm^2", style="") min_area
+@RoiManager rm
+@ResultsTable rt
+
 /*
    ana_pol4455_bks.groovy
 
@@ -12,38 +16,57 @@
    ==========  ===   ===================================================
    2019-06-02  JRM   Initial groovy prototype. Remember: you want to
                      learn scripting in Groovy...
+   2019-06-11  JRM   Added a string for minimum area, added a watershed
+   					 and burned the detected particles into the bks image,
+   					 saving the results as a png. Also saved the results
+   					 toa .csv file.
+
+   					 TODO: 1 - remove hard-coded paths.
+   					       2. think about how to handle a multi-image workflow.
     
 */
 
 import ij.IJ
 import ij.plugin.Duplicator
 
+// Start Clean. 1) reset ROI manager and Results table and clear the log
+rm.reset()
+rt.reset()
+
 IJ.log("\\Clear")
 
 def String img_path = img_dir + "/" + img_nam + img_tif
+println(img_path)
 
 def String str_sigma = sprintf("sigma=%d", gb_size)
-IJ.run("Gaussian Blur...", str_sigma)
+println(str_sigma)
 
-/*
-
-def String currentDir = new File(".").getAbsolutePath()
-def str_size = sprintf( formatted, name, pass )
-
-IJ.log(img_path)
-
+// start clean
 IJ.run("Close All")
-
 imp_ori = IJ.openImage(img_path)
+imp_out = imp_ori.duplicate()
+
+
+IJ.run(imp_ori, "Gaussian Blur...", str_sigma)
 imp_ori.show()
 
 IJ.run("Duplicate...", "title=work")
 def imp_wrk = IJ.image
 imp_wrk.show()
 
-String formatted = "sigma=$gb_size"
-/*
-IJ.setAutoThreshold(img, "Default")
-IJ.run(img, "Analyze Particles...", "  show=[Bare Outlines] include in_situ")
-img.show()
-*/
+IJ.setAutoThreshold(imp_wrk, "Default")
+IJ.run(imp_wrk, "Convert to Mask", "")
+IJ.run(imp_wrk, "Watershed", "")
+IJ.run("Set Measurements...", "area mean min perimeter fit shape redirect=None decimal=3")
+
+IJ.run(imp_wrk, "Analyze Particles...", "size="+min_area+"-Infinity circularity=0.50-1.00 show=Overlay display exclude include summarize add in_situ")
+imp_wrk.show()
+rt.show()
+rt.save("C:/Users/jrminter/Documents/git/tips/ImageJ/groovy/pol4455_bks_results.csv");
+rm.runCommand(imp_out,"Show None");
+rm.runCommand(imp_out,"Show All");
+rm.runCommand(imp_out,"Show All with labels");
+imp2 = imp_out.flatten();
+imp2.show()
+IJ.saveAs(imp2, "PNG", "C:/Users/jrminter/Documents/git/tips/ImageJ/groovy/POL-4455-16bit-Img01-bks-flattened.png");
+
--- ImageJ/ijm/radial_profile.ijm
+++ ImageJ/ijm/radial_profile.ijm
@@ -1,17 +1,21 @@
-// plot the radial profile of POL4455
-//
-// 2019-05-31 J. R. Minter
-//
-// uses plugin Radial_Profile.class in plugins folder
-// Source: https://imagej.nih.gov/ij/plugins/download/Radial_Profile.class
-// Written by Paul Baggethun. Last reported edit: 2009-05-14
-// see also: https://imagejdocu.tudor.lu/doku.php?id=macro:radial_distribution_function
-//
+/*
+*	plot the radial profile of POL4455
+*	uses plugin Radial_Profile.class in plugins folder
+*	
+*	   Date     Who  What
+*	----------  ---  -------------------------------------------
+*	2019-05-31  JRM  Initial prototype
+*	
+*	Uses plugin Radial_Profile.class in plugins folder
+*	Source: https://imagej.nih.gov/ij/plugins/download/Radial_Profile.class
+*	Written by Paul Baggethun. Last reported edit: 2009-05-14
+*	see also: https://imagejdocu.tudor.lu/doku.php?id=macro:radial_distribution_function
+*
+*/
 
 run("Close All");
-open("/Users/jrminter/Documents/git/tips/ImageJ/tif/POL-4455-16bit-Img01-bks.tif");
+open("C:/Users/jrminter/Documents/git/tips/ImageJ/tif/POL-4455-16bit-Img01-bks.tif");
 run("Median...", "radius=4");
 makeRectangle(0, 0, 1024, 1024);
-//run("Radial Profile", "x=512 y=512 radius=512");
 run("Radial Profile", "x=512 y=512 radius=512 use"); // use scale 
 saveAs("Results", "/Users/jrminter/Documents/git/tips/ImageJ/ijm/radial_profile_pol4455.csv");
