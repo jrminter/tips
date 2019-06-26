53d48468834227ebaccc134d89ef8fcb514062c4
John Minter <jrminter@gmail.com>
2019-06-25 22:09:32 -0400

    working test_virtual_stack

---
+++ ImageJ/py/test_virtual_stack/test_virtual_stack_pc.py
@@ -0,0 +1,34 @@
+"""
+test_virtual_stack_pc.py
+
+"""
+
+from ij import IJ, VirtualStack, ImagePlus
+import os
+
+git_home = os.getenv("GIT_HOME")
+
+img_dir = git_home + "/tips/ImageJ/py/test_virtual_stack/"
+print(img_dir)
+
+
+IJ.run("Close All")
+
+
+filtFiles = ["spheres-1.tif", "spheres-2.tif", "spheres-3.tif"]
+vs = None
+for f in filtFiles:
+	if vs is None:
+		path = img_dir + f
+		imp = IJ.openImage(path)
+		print(imp)
+		imp.show()
+		vs = VirtualStack(imp.width, imp.height, None, "/")
+		vs.addSlice(path)
+	else:
+		path = img_dir + f
+		vs.addSlice(path)
+		
+imPlus = ImagePlus("Stack from subdirectories", vs)
+print imPlus
+imPlus.show()
